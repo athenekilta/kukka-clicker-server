afa43ce77372eedf3818755eae6146760b2ad806
Oliver Mulari <oliver@feelbie.com>
2021-10-15 20:17:20 +0300

    authenticate socket server

--- src/controllers/KukkaClickerController.ts
+++ src/controllers/KukkaClickerController.ts
@@ -2,6 +2,9 @@
 import { Server } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import { ISocket } from "../interfaces/socket";
+import { logger } from "../utils/logger";
+import jwt from "jsonwebtoken";
+import config from "../utils/config";
 
 /**
  * Controller for live chat server instance
@@ -11,14 +14,46 @@ export class KukkaClickerController {
   MESSAGE_EXPIRATION_SECONDS: number;
   constructor(io: Server<DefaultEventsMap, DefaultEventsMap>) {
     this.io = io;
-    this.__init();
+    // this.__init();
   }
 
   /**
    * Tries to get user
    */
   async authenticate(socket: ISocket): Promise<string> {
-    // const user: IUser = await getUserFromServer(socket);
+    try {
+      // get token
+      let accessToken = null;
+      if (
+        socket.request.headers.authorization &&
+        socket.request.headers.authorization.startsWith("Bearer ")
+      ) {
+        accessToken = socket.request.headers.authorization.replace(
+          "Bearer ",
+          ""
+        );
+      }
+
+      if (accessToken) {
+        // has access token
+        // validate access token, throws error if expired
+        const decodedToken = jwt.verify(
+          accessToken,
+          config.ACCESS_TOKEN_SECRET_KEY
+        ) as {
+          username: string;
+          exp: number;
+        };
+
+        const username = decodedToken?.username || null;
+        // const user = await UserModel.findOne({ where: { username } });
+        socket.username = username;
+        return username;
+      }
+    } catch (error) {
+      logger({ error });
+      return null;
+    }
     return null;
   }
 
@@ -31,8 +66,8 @@ export class KukkaClickerController {
     // main method
     io.on("connection", async (socket: ISocket) => {
       // authenticate
-      const user = await controller.authenticate(socket);
-      if (user) {
+      const username = await controller.authenticate(socket);
+      if (username) {
         // join chat room
         socket.on("join", async ({ room_id }) => {
           // even unauthorized can join a room
